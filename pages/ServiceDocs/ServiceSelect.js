53ae5d0455acaa2888c41c4d63300e21abae7153
roboRiot <igor.a.sav@gmail.com>
2023-10-10 22:39:07 -0700

    began implmenting the file explorer

--- pages/ServiceDocs/ServiceSelect.js
+++ pages/ServiceDocs/ServiceSelect.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Form, Button, Card, Container } from "react-bootstrap";
-import styles from "../styles/Home.module.css";
+import { Form, FormControl, Button, Card, Container } from "react-bootstrap";
+import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-import { useAuth } from "../context/AuthUserContext";
+import { useAuth } from "../../context/AuthUserContext";
 
-import LoggedIn from "./LoggedIn";
+import LoggedIn from "./../LoggedIn";
 
 function simulateNetworkRequest() {
   return new Promise((resolve) => setTimeout(resolve, 2000));
@@ -26,15 +26,6 @@ function LoadingButton(type, name, route) {
   const handleClick = () => setLoading({ name: true });
 
   return (
-    // <a
-    //   class={"btn btn-" + type}
-    //   variant={type}
-    //   href={"/" + route}
-    //   disabled={isLoading.name}
-    //   onClick={!isLoading.name ? handleClick : null}
-    // >
-    //   {isLoading.name ? "Loading…" : name}
-    // </a>
     <a
       href={route}
       className={`btn btn-${type}`}
@@ -48,6 +39,17 @@ function LoadingButton(type, name, route) {
 
 export default function dashboard() {
   const { signOut } = useAuth();
+  const [search, setSearch] = useState();
+
+  const searchChangeHandler = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(search, showListSearch);
+    router.push(`WarehouseList?inputText=${search}&selectedType=${select}`);
+  };
+
   return (
     <LoggedIn>
       <Container
@@ -57,27 +59,29 @@ export default function dashboard() {
         <div className="w-100" style={{ maxWidth: "400px" }}>
           <Card className="align-items-center justify-content-center">
             <Card.Body>
-              <h2 className="text-center mb-4">Main Menu</h2>
+              <h2 className="text-center mb-4">Service Docs</h2>
               <div class="d-grid gap-3">
-                {LoadingButton(
-                  "primary",
-                  "Service Docs",
-                  "ServiceDocs/ServiceSelect"
-                )}
-
-                {/* {LoadingButton("secondary", "Logs", "notes/gKeep")} */}
+                {LoadingButton("primary", "Explorer", "./Explorer")}
 
                 {LoadingButton(
                   "secondary",
-                  "MagMon",
+                  "Google Keep",
                   "http://mri.advancedimagingparts.com/login"
                 )}
 
-                {LoadingButton(
-                  "primary",
-                  "Warehouse db",
-                  "Warehousedb/WarehouseSelect"
-                )}
+                <Form className="d-flex">
+                  <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    value={search}
+                    onChange={searchChangeHandler}
+                  />
+                  <Button variant="info" onClick={handleSubmit}>
+                    Search
+                  </Button>
+                </Form>
 
                 <Button variant="link" onClick={signOut}>
                   Sign out
