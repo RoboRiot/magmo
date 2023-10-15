92f1752613b8a6ee964e48c501c27580ccb3604f
igor savchenko <igor@igors-MacBook-Pro.local>
2023-10-14 23:14:33 -0700

    holy shit was that 4 or 5 hours of just dealing with compatiblities between outdated friking tools like the pdfViewer and the qr scanner, i mean i think the pdfViewer is okay, but that dang scanner is not letting me properly install mode code, ughhhh, well it works so im just happy there is that

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,11 @@ import '../styles/global.css'
 import Layout from '../components/Layout'
 import "bootstrap/dist/css/bootstrap.min.css"
 import { AuthUserProvider } from '../context/AuthUserContext';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // Include CSS for the annotation layer of PDF
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 function MyApp({ Component, pageProps }) {
   return (
