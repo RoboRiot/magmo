cb5a27c75671abd893fb79522b9dc7a886eef21b
Igor Savchenko <isavchenko@advancedimagingparts.com>
2024-02-08 14:29:08 -0800

    just checking

---
+++ pages/NewSearch/mainSearch.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useState, useRef } from "react";
+import { useRouter } from "next/router";
+import {
+  Form,
+  Button,
+  Card,
+  Container,
+  FormControl,
+  NavDropdown,
+  ButtonGroup,
+} from "react-bootstrap";
+import { useAuth } from "../../context/AuthUserContext";
+import LoggedIn from "../LoggedIn";
+
+export default function mainSearch() {
+  return (
+    <LoggedIn>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card className="align-items-center justify-content-center">
+            <Card.Body>
+              <h2 className="text-center mb-4">New Search</h2>
+              <ButtonGroup>
+                <div class="dropdown">
+                  <button
+                    class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown button one
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div class="dropdown">
+                  <button
+                    class="btn btn-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown button two
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <a class="dropdown-item" href="#">
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </ButtonGroup>
+            </Card.Body>
+          </Card>
+        </div>
+      </Container>
+    </LoggedIn>
+  );
+}
