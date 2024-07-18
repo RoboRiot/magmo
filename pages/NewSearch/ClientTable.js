e7b59f79d684ae065f7088520d4cacee97d9a59d
roboRiot <igor.a.sav@gmail.com>
2024-07-18 11:51:39 -0700

    not sure if there was any changes but im beginning to work on the single item script

--- pages/NewSearch/ClientTable.js
+++ pages/NewSearch/ClientTable.js
@@ -1,52 +1,57 @@
+// components/ClientTable.js
 import React from "react";
-import { Table, Button } from "react-bootstrap";
+import { Table, Button, FormControl } from "react-bootstrap";
 import styles from "../../styles/ClientTable.module.css";
 
-export default function ClientTable({
+const ClientTable = ({
   clients,
   onSelectClient,
   onInfoClick,
   clearSelection,
-}) {
+}) => {
   return (
-    <Table striped bordered hover size="sm" className={styles.clientTable}>
-      <thead>
-        <tr>
-          <th>Client Name</th>
-          <th>Info</th>
-          <th>Select</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td colSpan="3" style={{ textAlign: "center" }}>
-            <Button variant="secondary" onClick={clearSelection}>
-              Clear Selection
-            </Button>
-          </td>
-        </tr>
-        {clients.map((client) => (
-          <tr key={client.id}>
-            <td>{client.name}</td>
-            <td>
-              <Button
-                variant="info"
-                onClick={() => onInfoClick(client.id, client.name)}
-              >
-                Info
-              </Button>
-            </td>
-            <td>
-              <Button
-                variant="primary"
-                onClick={() => onSelectClient(client.name)}
-              >
-                Select
+    <>
+      <Table striped bordered hover size="sm" className={styles.clientTable}>
+        <thead>
+          <tr>
+            <th>Client Name</th>
+            <th>Info</th>
+            <th>Select</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td colSpan="3" style={{ textAlign: "center" }}>
+              <Button variant="secondary" onClick={clearSelection}>
+                Clear Selection
               </Button>
             </td>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+          {clients.map((client) => (
+            <tr key={client.id}>
+              <td>{client.name}</td>
+              <td>
+                <Button
+                  variant="info"
+                  onClick={() => onInfoClick(client.id, client.name)}
+                >
+                  Info
+                </Button>
+              </td>
+              <td>
+                <Button
+                  variant="primary"
+                  onClick={() => onSelectClient(client.name)}
+                >
+                  Select
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
-}
+};
+
+export default ClientTable;
