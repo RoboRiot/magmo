e73baa81f06ec85ed8e6fd12dd642014a97b4157
Igor Savchenko <isavchenko@advancedimagingparts.com>
2024-09-10 12:52:56 -0700

    fixed some bugs

--- pages/NewSearch/item/[id]/InfoModal.js
+++ pages/NewSearch/item/[id]/InfoModal.js
@@ -2,18 +2,19 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const InfoModal = ({ show, handleClose, itemName }) => {
+const InfoModal = ({ show, handleClose, itemName, dimensions, price,  }) => {
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Item Info</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p><strong>Name:</strong> {itemName}</p>
-        <p><strong>Dimensions:</strong> {itemName}</p>
-        <p><strong>Price:</strong> {itemName}</p>
-        <p><strong>Frequency:</strong> {itemName}</p>
-        <p><strong>Usage past 1 year:</strong> {itemName}</p>
+        <p><strong>Name:</strong> "RTM"</p>
+        <p><strong>Dimensions:</strong> "6x3x1"</p>
+        <p><strong>Price:</strong> "$300"</p>
+        <p><strong>Frequency of item:</strong> "4"</p>
+        <p><strong>Frequency of Machine:</strong> "13"</p>
+        <p><strong>Usage past 1 year:</strong> "2"</p>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
